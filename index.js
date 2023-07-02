ba16cbe37d14c05d7ecc598f38055bd4c99ee005
jfmemon <jannatul15-2497@diu.edu.bd>
2023-07-02 18:53:17 +0600

    specific data read by query parameter with specific email

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
+        const ordersCollection = client.db('geniusCar').collection('orders');
 
+        // db theke data(all) read korbe and api create korbe
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
@@ -31,11 +33,36 @@ async function run() {
             res.send(services);
         })
 
+
+        // db theke data(one) read korbe and api create korbe
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        })
+
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+
+            if(req.query.email) {
+                query = {
+                    email: req.query.email,
+                }
+            }
+            
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        
+        // client site theke data anbe and POST API create korbe
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await ordersCollection.insertOne(orders);
+            res.send(result);
         })
     }
 
